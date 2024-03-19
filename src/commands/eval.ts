51532bf2e05466a8b2ad6333a5233f4d3bd31e11
TÆMBØ <kuneshr123@gmail.com>
2024-03-19 01:16:36 -0700

    Fix issue with editing reply without webhook

--- src/commands/eval.ts
+++ src/commands/eval.ts
@@ -1,18 +1,20 @@
 import * as Discord from "@discordjs/core/http-only";
-import { performance } from "perf_hooks";
+import * as Builders from "@discordjs/builders";
+import { performance } from "node:perf_hooks";
 import { setTimeout as sleep } from "node:timers/promises";
-import util from "node:util";
-import { ActionRowBuilder, ButtonBuilder, EmbedBuilder, SlashCommandBuilder } from "@discordjs/builders";
-import * as utilities from "../utilities.js";
+import { formatWithOptions } from "node:util";
 import { Command } from "../structures/command.js";
+import * as utils from "../utilities.js";
 
 export default new Command({
     async run(app, interaction, options) {
+        await app.api.interactions.defer(interaction.id, interaction.token);
+
         sleep;
         const code = options.getString("code", true);
         const depth = options.getInteger("depth") ?? 1;
         const useAsync = Boolean(options.getBoolean("async", false));
-        const embed = new EmbedBuilder()
+        const embed = new Builders.EmbedBuilder()
             .setTitle("__Eval__")
             .setColor(4203516)
             .addFields({ name: "Input", value: `\`\`\`js\n${code.slice(0, 1010)}\`\`\`` });
@@ -22,7 +24,7 @@ export default new Command({
         try {
             output = await eval(useAsync ? `(async () => { ${code} })()` : code);
         } catch (err: any) {
-            utilities.log("Red", err);
+            utils.log("Red", err);
             
             embed
                 .setColor(16711680)
@@ -33,31 +35,35 @@ export default new Command({
 
             const msgPayload = {
                 embeds: [embed.toJSON()],
-                components: [new ActionRowBuilder<ButtonBuilder>().addComponents(new ButtonBuilder().setCustomId("stack").setStyle(Discord.ButtonStyle.Primary).setLabel("Stack")).toJSON()],
+                components: [new Builders.ActionRowBuilder<Builders.ButtonBuilder>().addComponents(new Builders.ButtonBuilder()
+                    .setCustomId("stack")
+                    .setStyle(Discord.ButtonStyle.Primary)
+                    .setLabel("Stack")
+                ).toJSON()],
             };
 
-            const msg = await app.api.interactions.reply(interaction.id, interaction.token, msgPayload)
-                .then(() => app.api.webhooks.getMessage(app.config.clientId, interaction.token, "@original"))
-                .catch(() => app.api.channels.createMessage(interaction.channel.id, msgPayload));
+            await app.api.interactions.editReply(app.config.clientId, interaction.token, msgPayload);
 
-            new utilities.Collector(app, { filter: (int) => (int.member ?? int).user!.id === (interaction.member ?? interaction).user!.id, max: 1 })
+            new utils.Collector(app, { filter: int => (int.member ?? int).user!.id === (interaction.member ?? interaction).user!.id, max: 1 })
                 .on("collect", int => app.api.interactions.reply(int.id, int.token, { content: `\`\`\`\n${err.stack.slice(0, 1950)}\`\`\`` }))
-                .on("end", () => app.api.channels.editMessage(interaction.channel.id, msg.id, { components: [] }));
+                .on("end", () => app.api.interactions.editReply(app.config.clientId, interaction.token, { components: [] }));
 
             return;
         }
 
         // Output manipulation
         if (typeof output === "object") {
-            output = "js\n" + util.formatWithOptions({ depth }, "%O", output);
+            output = "js\n" + formatWithOptions({ depth }, "%O", output);
         } else output = "\n" + String(output);
 
-        embed.addFields({ name: `Output • ${(performance.now() - now).toFixed(5)}ms`, value: `\`\`\`${output.slice(0, 1016)}\n\`\`\`` });
+        embed.addFields({
+            name: `Output • ${(performance.now() - now).toFixed(5)}ms`,
+            value: `\`\`\`${output.slice(0, 1016)}\n\`\`\``
+        });
 
-        await app.api.interactions.reply(interaction.id, interaction.token, { embeds: [embed.toJSON()] })
-            .catch(() => app.api.channels.createMessage(interaction.channel.id, { embeds: [embed.setFooter({ text: "Reply timeout" }).toJSON()] }));
+        await app.api.interactions.editReply(app.config.clientId, interaction.token, { embeds: [embed.toJSON()] });
     },
-    data: new SlashCommandBuilder()
+    data: new Builders.SlashCommandBuilder()
         .setName("eval")
         .setDescription("Execute code within the application")
         .addStringOption(x => x
