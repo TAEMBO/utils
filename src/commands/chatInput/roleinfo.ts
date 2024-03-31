d870f440970acf65e1ce11592c44af22cfe53173
TÆMBØ <kuneshr123@gmail.com>
2024-03-31 00:15:33 -0700

    Add roleinfo command

---
+++ src/commands/chatInput/roleinfo.ts
@@ -0,0 +1,20 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { formatWithOptions } from "node:util";
+import { ChatInputCommand } from "../../structures/index.js";
+
+export default new ChatInputCommand({
+    async run(app, interaction, options) {
+        const role = options.getRole("role", true);
+
+        await app.api.interactions.reply(interaction.id, interaction.token, {
+            content: `\`\`\`js\n${formatWithOptions({ depth: 5 }, "%O", role).slice(0, 1990)}\`\`\``
+        });
+    },
+    data: new SlashCommandBuilder()
+        .setName("roleinfo")
+        .setDescription("Get API data on a role")
+        .addRoleOption(x => x
+            .setName("role")
+            .setDescription("The role to get API data on")
+            .setRequired(true))
+});
