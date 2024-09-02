e0b11cee36c99b340fe805fb8b79107716fca0bb
TAEMBO <kuneshr123@gmail.com>
2024-09-02 13:34:37 -0700

    feat: Escape codeblocks in message content

--- src/commands/contextMenu/messageInfo.ts
+++ src/commands/contextMenu/messageInfo.ts
@@ -1,10 +1,13 @@
 import { ApplicationCommandType } from "@discordjs/core/http-only";
 import { formatWithOptions } from "node:util";
 import { ContextMenuCommand } from "#structures";
+import { escapeCodeBlock } from "@discordjs/builders";
 
 export default new ContextMenuCommand<"message">({
     async run(app, interaction, options) {
         const targetMessage = options.getTargetMessage();
+
+        targetMessage.content = escapeCodeBlock(targetMessage.content);
 
         await app.api.interactions.reply(interaction.id, interaction.token, {
             content: `\`\`\`js\n${formatWithOptions({ depth: 5 }, "%O", targetMessage).slice(0, 1990)}\`\`\``,
