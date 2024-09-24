c76f133e32062fe069ea9272e635f22dd5cec511
TAEMBO <kuneshr123@gmail.com>
2024-09-23 21:15:21 -0700

    refactor: Improve readability

--- src/util/formatTime.ts
+++ src/util/formatTime.ts
@@ -1,41 +1,49 @@
+const timeNames = [
+    { name: "year",   length: 1_000 * 60 * 60 * 24 * 365 },
+    { name: "month",  length: 1_000 * 60 * 60 * 24 * 30 },
+    { name: "week",   length: 1_000 * 60 * 60 * 24 * 7 },
+    { name: "day",    length: 1_000 * 60 * 60 * 24 },
+    { name: "hour",   length: 1_000 * 60 * 60 },
+    { name: "minute", length: 1_000 * 60 },
+    { name: "second", length: 1_000 }
+];
+
 /**
- * 
+ * Formats a given millisecond integer to human-readable text 
  * @param integer The integer in milliseconds to format
  * @param accuracy The number of layers of accuracy to be presented in the return, e.g. `1` for "1h" or `2` for "1h, 1m"
- * @param options `longNames` - whether to display amounts as compact or not, e.g. (`s`) or (`seconds`). `commas` - Whether to space each amount with a comma or not
+ * @param options `longNames` - whether to display amounts as compact or not, e.g. (`s`) or (`seconds`). `commas` - Whether to separate each accuracy with a comma or not
  * @returns The formatted output
  */
-export function formatTime(integer: number, accuracy = 1, options?: { longNames?: boolean, commas?: boolean }) {
-    const timeNames = [
-        { name: "year",   length: 1_000 * 60 * 60 * 24 * 365 },
-        { name: "month",  length: 1_000 * 60 * 60 * 24 * 30 },
-        { name: "week",   length: 1_000 * 60 * 60 * 24 * 7 },
-        { name: "day",    length: 1_000 * 60 * 60 * 24 },
-        { name: "hour",   length: 1_000 * 60 * 60 },
-        { name: "minute", length: 1_000 * 60 },
-        { name: "second", length: 1_000 }
-    ];
+export function formatTime(integer: number, accuracy = 1, options: { longNames?: boolean, commas?: boolean } = {}) {
+    let ms = integer;
     let achievedAccuracy = 0;
     let text = "";
 
     for (const timeName of timeNames) {
         if (achievedAccuracy >= accuracy) break;
-        
-        const fullTimelengths = Math.floor(integer / timeName.length);
+
+        const fullTimelengths = Math.floor(ms / timeName.length);
 
         if (!fullTimelengths) continue;
 
         achievedAccuracy++;
-        text += fullTimelengths + (options?.longNames ? (" " + timeName.name + (fullTimelengths === 1 ? "" : "s")) : timeName.name.slice(0, timeName.name === "month" ? 2 : 1)) + (options?.commas ? ", " : " ");
-        integer -= fullTimelengths * timeName.length;
+        text +=
+            fullTimelengths
+            + (
+                options.longNames
+                    ? (" " + timeName.name + (fullTimelengths === 1 ? "" : "s"))
+                    : timeName.name.slice(0, timeName.name === "month" ? 2 : 1))
+            + (options.commas ? ", " : " ");
+        ms -= fullTimelengths * timeName.length;
     }
 
-    if (!text) text = integer + (options?.longNames ? " milliseconds" : "ms") + (options?.commas ? ", " : "");
+    if (!text) text = ms + (options.longNames ? " milliseconds" : "ms") + (options.commas ? ", " : "");
 
-    if (options?.commas) {
+    if (options.commas) {
         text = text.slice(0, -2);
 
-        if (options?.longNames) {
+        if (options.longNames) {
             const textArr = text.split("");
 
             textArr[text.lastIndexOf(",")] = " and";
